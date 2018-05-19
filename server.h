d7caac30c5d5d4bdc599424cae74228812d4fbe6
cxt562117562 <38999473+cxt562117562@users.noreply.github.com>
2018-05-20 01:25:30 +0800

    2018/5/20/1：25

--- server.h
+++ server.h
@@ -21,18 +21,18 @@ private slots:
 		serve_airconditioner toolair = new serve_airconditioner();
         toolair.air_socket = server_sever->nextPendingConnection();
 		serve_airconditionerptr.push_back(toolair);
-		QObject::connect(toolair->air_socket, &QTcpSocket::readyRead, this, server::server_receive);
-		QObject::connect(toolair->air_socket, &QTcpSocket::disconnected, this, server::server_disconnected);
+        QObject::connect(toolair.air_socket, SIGNAL(QTcpSocket::readyRead()), this, SLOT(server::server_receive()));
+        QObject::connect(toolair.air_socket, SIGNAL(QTcpSocket::disconnected()), this, SLOT(server::server_disconnected()));
 	}
 	void server_receive()//接收
 	{
 		QByteArray buffer;
-        buffer = ((QTcpSocket*)sender())->readAll();
+        buffer = ((QTcpSocket*)(QObject::sender()))->readAll();
 		//此处格式转化解析报文
 	}
 	void server_disconnect()//断开连接
 	{
-        QTcpSocket* toolsocket=(QTcpSocket*)sender();
+        QTcpSocket* toolsocket=(QTcpSocket*)(QObject::sender());
 		for (int i = 0; i < serve_airconditionerptr.size(); ++i)
 			if (serve_airconditionerptr[0].air_socket == toolsocket)
                 this->serve_airconditionerptr.erase(serve_airconditionerptr[0].begin() + i, serve_airconditionerptr[0].begin() + i + 1);
@@ -41,19 +41,19 @@ public:
 	server()//构造函数
 	{
 		server_sever = new QTcpServer();
-		server_server->listen(QHostAddress::Any, port);
-		QObject::connect(server_sever, &QTcpServer::newConnection, this, &server::server_new_connect);
+        server_sever->listen(QHostAddress::Any, port);//
+        QObject::connect(server_sever, SIGNAL(QTcpServer::newConnection()), this, SLOT(server::server_new_connect()));
 	}
-	//定期发送（未具有定期功能）
+    //定期发送（未具有定期功能）
     void server_send()
 	{
 		QByteArray buffer;
 
 		for(int i=0;i<serve_airconditionerptr.airconditionerptr.size();++i)
-			if (serve_airconditionerptr[i].gettheinforable == 1)
+            if (serve_airconditionerptr[i].gettheinforable() == 1)
 			{
 				//利用serve_airconditionerptr[i]的get函数获取报文信息
-				serve_airconditionerptr[i]->write(data);
+                serve_airconditionerptr[i].air_socket->write(data);
 			}
 	}
 	
